c915e66adb67bf33732f4ccc1fb428c775aeba9c
Vincent Ratzke <vratzke@brockhaus-ag.de>
2024-07-27 19:39:43 +0200

    added gitignore

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
-dailyDeal = "https://www.digitec.ch/en/daily-deal"
-span = "sc-812f8453-1 fMoCQC"
-spanName = "sc-2e9036-0 cNsIaf sc-66a042f4-11 cQwspH"
-Wishlist = ["https://www.digitec.ch/en/s1/product/logitech-mx-master-3s-wireless-mouse-20987854?ip=logi"]
-soup = BeautifulSoup(requests.get(dailyDeal).content, "html.parser")
+try:
+    dailyDeal = "https://www.digitec.ch/en/daily-deal"
+    span = "sc-812f8453-1 fMoCQC"
+    spanName = "sc-2e9036-0 cNsIaf sc-66a042f4-11 cQwspH"
+    Wishlist = ["https://www.digitec.ch/en/s1/product/logitech-mx-master-3s-wireless-mouse-20987854?ip=logi"]
+    soup = BeautifulSoup(requests.get(dailyDeal).content, "html.parser")
 
-print("Daily Deal:")
-print(soup.find(class_=spanName).text)
-print(soup.find(class_=span).text)
+    print("Daily Deal:")
+    print(soup.find(class_=spanName).text)
+    print(soup.find(class_=span).text)
 
-print("Whislist:")
-for item in Wishlist:
-    soup = BeautifulSoup(requests.get(item).content, "html.parser")
-    item = soup.find(class_="sc-12fcbcc6-0 kHeaGZ").text
-    price = soup.find(class_="sc-168a3b5-5 dHyqgD").text
-    print(f"Item: {item}, with price: {price}")
-    if soup.find(class_="sc-b9b85e36-0 sc-354b679-0 cTpMKM coQedg"):
-        discount = soup.find(class_="sc-b9b85e36-0 sc-354b679-0 cTpMKM coQedg").text
-        print(f"with discount of: {discount}")
+    print("Whislist:")
+    for item in Wishlist:
+        soup = BeautifulSoup(requests.get(item).content, "html.parser")
+        item = soup.find(class_="sc-12fcbcc6-0 kHeaGZ").text
+        price = soup.find(class_="sc-168a3b5-5 dHyqgD").text
+        print(f"Item: {item}, with price: {price}")
+        if soup.find(class_="sc-b9b85e36-0 sc-354b679-0 cTpMKM coQedg"):
+            discount = soup.find(class_="sc-b9b85e36-0 sc-354b679-0 cTpMKM coQedg").text
+            print(f"with discount of: {discount}")
+except Exception as e:
+    print(f"Error occurred: {e}")
 
 
 
